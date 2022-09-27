85516292e4df2fe9638721bcee4bd238ec2e2b60
Anand <anand_jbk@yahoo.com>
2022-09-27 20:09:11 +0100

    refactoring changes

--- utils/src/main/java/com/assessment/sj/utils/VersionComparator.java
+++ utils/src/main/java/com/assessment/sj/utils/VersionComparator.java
@@ -8,21 +8,21 @@ public class VersionComparator implements Comparator<String> {
 
     @Override
     public int compare(String firstVersion, String secondVersion) {
-        String[] FirstVersionParts = firstVersion.split(PARTS_SEPARATOR);
+        String[] firstVersionParts = firstVersion.split(PARTS_SEPARATOR);
         String[] secondVersionParts = secondVersion.split(PARTS_SEPARATOR);
 
-        int numberOfPartsInShorterVersion = Math.min(FirstVersionParts.length, secondVersionParts.length);
+        int numberOfPartsInShorterVersion = Math.min(firstVersionParts.length, secondVersionParts.length);
 
         int i = 0;
         int partialResult = 0;
         while (i < numberOfPartsInShorterVersion){
-            partialResult = Integer.compare(Integer.parseInt(FirstVersionParts[i]), Integer.parseInt(secondVersionParts[i]));
+            partialResult = Integer.compare(Integer.parseInt(firstVersionParts[i]), Integer.parseInt(secondVersionParts[i]));
             if(partialResult != 0){
                 break;
             }
             i++;
         }
 
-        return partialResult != 0 ? partialResult : Integer.compare(FirstVersionParts.length, secondVersionParts.length);
+        return partialResult != 0 ? partialResult : Integer.compare(firstVersionParts.length, secondVersionParts.length);
     }
 }
